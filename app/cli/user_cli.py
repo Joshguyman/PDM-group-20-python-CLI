877d96c3c49a65e186d60cb25766bc58341678b5
jadene17 <jadenespejo17@gmail.com>
2025-03-20 16:40:44 -0400

    help message bruh

--- app/cli/user_cli.py
+++ app/cli/user_cli.py
@@ -2,15 +2,31 @@
 from app.services.user_services import *
 import getpass
 
-
 # Session variables
 session_username: str
 session_password: str
 
 credentials_loaded: bool = False
 
+def help_message():
+    print(
+          "Create a Collection (CC <collection_name>"
+          "View Your Collections (CV)"
+          "Add Games to your Collection (CA <collection_name> <space_separated_list_of_game_titles>) "
+          "Delete Games from your Collection (CA <collection_name> <space_separated_list_of_game_titles>) "
+          "Search for game by Name (SN <game_name>) "
+          "Search for game by Platform (SP <platform_name>) "
+          "Search for game by Release Date (SR <release_date>) "
+          "Search for game by Developer (SD <dev_name>) "
+          "Search for game by Publisher (SP <pub_name>) "
+          "Search for game by Price (SPR <price>) "
+          "Search for game by Genre (SG <genre_name>) "
+    )
+
 def session_loop():
     global credentials_loaded
+    global session_username
+    global session_password
     # Pre-loop login/signup
     while (not credentials_loaded):
         login_decision = input("Log in or Sign up (L/S): ")
@@ -21,7 +37,9 @@ def session_loop():
 
             if (session_password == session_password_confirmation):
                 print("passwords match")
-                sign_in(session_username, session_password)
+                uid = sign_in(session_username, session_password)
+                if (uid):
+
                 # TODO: set credentials_loaded to true under the condition that sign_in worked then proceed to live loop
             else:
                 print("Passwords did not match!")
@@ -35,7 +53,7 @@ def session_loop():
             session_email = input("Enter email: ")
             if (session_password == session_password_confirmation):
                 print("passwords match")
-                # create_account(session_username, session_password, session_f_name, session_l_name, session_email)
+                create_account(session_username, session_password, session_f_name, session_l_name, session_email)
                 # TODO: bring back to start of loop (should already happen) on the condition there was not already-
                 # - a present username or email (this should happen in utls folder or model folder)
             else:
