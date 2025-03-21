bf7487752482d03936057b60ea9169b0affb5dc9
Joshguyman <61127872+Joshguyman@users.noreply.github.com>
2025-03-21 15:51:10 -0400

    more implementation for services
    finished and tested add to collection, remove from collection, and search games

--- app/services/user_services.py
+++ app/services/user_services.py
@@ -5,7 +5,10 @@ sign_in: sign in with a username and password
 @:param password
 @:return uid of the user
 """
+from app.models.collection_model import *
 from app.models.user_model import *
+from app.models.videogame_model import *
+from app.utils.format import format_videogame_result
 
 
 def sign_in(conn, username, password):
@@ -35,7 +38,7 @@ def create_account(conn, username, password, first_name, last_name, email):
         return None
 
     print("Successfully created account")
-    return result[0]
+    return result
 
 """
 new_collection: create a new collection
@@ -57,8 +60,25 @@ add_games_to_collection: add games to an existing collection owned by the user
 @:param games -> list of game titles to add to the collection
 @:return None (might change idk)
 """
-def add_games_to_collection(colid, uid, games:list):
-    return None
+def add_games_to_collection(conn, colid, uid, games:list):
+    collection = get_collection_by_id(conn, colid)
+    if check_collection_owner(conn, uid, colid):
+        for game in games:
+            vid = get_videogame_id(conn, game)
+            if vid is None:
+                print(f"No such game \"{game}\"")
+                continue
+            vid = vid[0]
+            if check_game_in_collection(conn, colid, vid):
+                print(f"\"{game}\" is already in collection \"{collection}\"")
+                continue
+            add_game(conn, colid, vid)
+            print(f"Successfully added \"{game}\" to collection \"{collection}\"")
+        contents = get_games_in_collection(conn, colid)
+        print(f"Collection \"{collection}\" now contains {contents}")
+        return
+    print(f"User does not own collection \"{collection}\"")
+    return
 """
 remove_games_from_collection: remove games from an existing collection owned by the user
 @:param name -> name of the existing collection
@@ -66,16 +86,60 @@ remove_games_from_collection: remove games from an existing collection owned by 
 @:param games -> list of game titles to remove from the collection
 @:return None (might change idk)
 """
-def remove_games_from_collection(colid, uid, games:list):
-    return None
+def remove_games_from_collection(conn, colid, uid, games:list):
+    collection = get_collection_by_id(conn, colid)
+    if check_collection_owner(conn, uid, colid):
+        for game in games:
+            vid = get_videogame_id(conn, game)
+            if vid is None:
+                print(f"No such game \"{game}\"")
+                continue
+            vid = vid[0]
+            if not check_game_in_collection(conn, colid, vid):
+                print(f"\"{game}\" is not in collection \"{collection}\"")
+                continue
+            remove_game(conn, colid, vid)
+            print(f"Successfully removed \"{game}\" from collection \"{collection}\"")
+        contents = get_games_in_collection(conn, colid)
+        print(f"Collection \"{collection}\" now contains {contents}")
+        return
+    print(f"User does not own collection \"{collection}\"")
+    return
 """
 search_videogame: search a video game by title, platform, release date, developer, price, genre
 @:param val -> the value to be searched
 @:param searchtype -> int specifying the search type (i.e 1 for title, 2 for platform, 3 for release date, 4 for developer, 5 for genre)
 @:return list of videogames with the specified value
 """
-def search_videogame(val, searchtype):
-    return None
+def search_videogame(conn, val, searchtype):
+    result = []
+    st = str
+    match searchtype:
+        case "title":
+            result = get_videogame_by_title(conn, val)
+            st = "Title"
+        case "platform":
+            result = get_videogame_by_platform(conn, val)
+            st = "Platform"
+        case "release date":
+            result = get_videogame_by_release_date(conn, val)
+            st = "Release Date"
+        case "developer":
+            result = get_videogame_by_dev_name(conn, val)
+            st = "Developer"
+        case "publisher":
+            result = get_videogame_by_pub_name(conn, val)
+            st = "Publisher"
+        case "genre":
+            result = get_videogame_by_genre_name(conn, val)
+            st = "Genre"
+        case "price":
+            result = get_videogame_by_price(conn, val)
+            st = "Price"
+        case _:
+            print("Unrecognized search type")
+    format_videogame_result(result,val, st)
+
 """
 play_videogame: start playing a video game
 @:param name -> the name of the game to be played
